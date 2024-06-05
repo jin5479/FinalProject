decc58f13e9903f214f5e7151fed1808b63515f7
Lacheln1 <psionict@naver.com>
2024-06-05 16:17:06 +0900

    이용 관련 안내 홈페이지 제작

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,13 @@
+import "../assets/css/buttonStyle.css";
 
-import '../assets/css/buttonStyle.css'
-
-export default function Button(props){
-    
-    return(
-        <button onClick={() => window.location.href = props.url}>{props.title}</button>
-    )
-    
+export default function Button(props) {
+  return (
+    <>
+      <button onClick={() => (window.location.href = props.url)}>
+        
+        <img src={props.img} alt="이미지"></img>
+        <span>{props.title}</span>
+      </button>
+    </>
+  );
 }
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,6 +1,9 @@
 import roomImg from '../assets/img/room1.jpg'
 import '../assets/css/mainPageCSS.css'
 import Button from '../components/Button'
+import noticeIcon from '../assets/img/noticeIcon.png'
+import scheduleIcon from '../assets/img/scheduleIcon.png'
+import writeIcon from '../assets/img/writeIcon.png'
 
 export default function MainPage(){
     return(
@@ -10,9 +13,9 @@ export default function MainPage(){
             </div>
 
             <div className='noticeContainer'>
-                <Button title="이용 관련 안내" url="https://www.naver.com/"/>
-                <Button title="예약 현황" url="https://www.google.co.kr/"/>
-                <Button title="예약 하기" url="https://section.cafe.naver.com/ca-fe/home"/>
+                <Button title="이용 관련 안내" url="https://www.naver.com/" img={noticeIcon}/>
+                <Button title="예약 현황" url="https://www.google.co.kr/" img={scheduleIcon}/>
+                <Button title="예약 하기" url="https://section.cafe.naver.com/ca-fe/home" img={writeIcon}/>
             </div>
         </>
         
