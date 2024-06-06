1eb1d7a05af9ecc7d6189f70e5910c6c57ea5ef7
Lacheln1 <psionict@naver.com>
2024-06-06 16:20:46 +0900

    메인페이지 보강
    사진 여러개 추가해서 좌우로 누를수 있게 , 좌우 버튼은 스타일 수정 예정

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,15 +1,38 @@
-import roomImg from '../assets/img/room1.jpg'
-import '../assets/css/mainPageCSS.css'
-import Button from '../components/Button'
-import noticeIcon from '../assets/img/noticeIcon.png'
-import scheduleIcon from '../assets/img/scheduleIcon.png'
-import writeIcon from '../assets/img/writeIcon.png'
+import React, { useState } from 'react';
+import roomImg from '../assets/img/room1.jpg';
+import roomImg2 from '../assets/img/room2.jpg';
+import roomImg3 from '../assets/img/room3.jpg';
+import '../assets/css/mainPageCSS.css';
+import Button from '../components/Button';
+import noticeIcon from '../assets/img/noticeIcon.png';
+import scheduleIcon from '../assets/img/scheduleIcon.png';
+import writeIcon from '../assets/img/writeIcon.png';
 
-export default function MainPage(){
-    return(
+const images = [roomImg, roomImg2, roomImg3];
+
+export default function MainPage() {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const handleMoveLeft = () => {
+        setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+    };
+
+    const handleMoveRight = () => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    };
+
+    return (
         <>
             <div className='roomContainer'>
-                <img className='roomImgStyle' src={roomImg} alt="이미지"/>
+                <button onClick={handleMoveLeft} className='moveButton'>Left</button>
+                <div className='imgWrapper'>
+                    <img 
+                        className='roomImgStyle' 
+                        src={images[currentIndex]} 
+                        alt="이미지" 
+                    />
+                </div>
+                <button onClick={handleMoveRight} className='moveButton'>Right</button>
             </div>
 
             <div className='noticeContainer'>
@@ -18,6 +41,5 @@ export default function MainPage(){
                 <Button title="예약 하기" url="https://section.cafe.naver.com/ca-fe/home" img={writeIcon}/>
             </div>
         </>
-        
-    )
+    );
 }
