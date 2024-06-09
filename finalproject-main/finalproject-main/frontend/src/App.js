4b8e2f8d415d4595b1fee0e2afe5062e126dfffc
jingi <122077048+jin5479@users.noreply.github.com>
2024-06-09 20:35:20 +0900

    Add files via upload
    FinalRepository.java
    ReservationService.java
    ReservationServiceImpl.java
    ReservationController.java
    예약관련

---
+++ finalproject-main/finalproject-main/frontend/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Header from './components/Header';
+import MainPage from './pages/MainPage';
+
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <MainPage/>
+    </>
+  );
+}
+
+export default App;
