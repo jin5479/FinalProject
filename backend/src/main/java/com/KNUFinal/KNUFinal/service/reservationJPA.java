ec93e3539d4a31286b6c86bb9c683e37e5de27f0
sulbos-GP <gs2576@naver.com>
2024-06-12 00:11:38 +0900

    Reservation CRUD 구현

--- backend/src/main/java/com/KNUFinal/KNUFinal/service/reservationJPA.java
+++ backend/src/main/java/com/KNUFinal/KNUFinal/service/reservationJPA.java
@@ -1,6 +1,7 @@
 package com.KNUFinal.KNUFinal.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,7 +23,7 @@ public class reservationJPA implements reservationService {
         Reservation reservation = new Reservation();
         reservation.setUsername(reservationDTO.getUsername());
         reservation.setDate(reservationDTO.getDate());
-        reservation.setPassword(reservation.getPassword());
+        reservation.setPassword(reservationDTO.getPassword());
         repository.save(reservation);
     }
 
@@ -30,6 +31,28 @@ public class reservationJPA implements reservationService {
     public List<ReservationDTO> reservationList() {
        return repository.findAll().stream().map(ReservationDTO::new).collect(Collectors.toList());
     }
-    
+
+    @Override
+    public void  deleteReservation(long id) {
+        repository.deleteById(id);
+        System.out.print("삭제 완료"+id);
+    }
+
+    @Override
+    public Reservation  updateReservation(long id,  ReservationDTO reservationDTO){
+        //Optional 기능을 통해 값이 없는 null인 경우 Optional.Empty를 반환 해 예외 방지
+        Optional<Reservation> optionalReservation = repository.findById(id);
+
+        if (optionalReservation.isPresent()) {
+            Reservation reservation = optionalReservation.get();
+            reservation.setUsername(reservationDTO.getUsername());
+            reservation.setDate(reservationDTO.getDate());
+            reservation.setPassword(reservationDTO.getPassword());
+            return repository.save(reservation);
+        } else {
+            throw new RuntimeException("Reservation not found with id " + id);
+        }
+    }
+
 
 }
--- backend/src/main/java/com/KNUFinal/KNUFinal/service/reservationService.java
+++ backend/src/main/java/com/KNUFinal/KNUFinal/service/reservationService.java
@@ -4,10 +4,13 @@ package com.KNUFinal.KNUFinal.service;
 
 import java.util.List;
 
+import com.KNUFinal.KNUFinal.model.Reservation;
 import com.KNUFinal.KNUFinal.model.ReservationDTO;
 
 public interface reservationService {
     
     List<ReservationDTO> reservationList();
     void createReservation(ReservationDTO reserveDTO);
+    void deleteReservation(long id);
+    Reservation updateReservation(long id, ReservationDTO reservationDTO);
 }
