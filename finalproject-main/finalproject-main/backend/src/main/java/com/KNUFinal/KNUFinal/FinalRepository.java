553da0b072f504aa5e43db2aaedee0447c56c8be
Lacheln1 <psionict@naver.com>
2024-06-12 11:35:16 +0900

    필요없는거 삭제

--- finalproject-main/finalproject-main/backend/src/main/java/com/KNUFinal/KNUFinal/FinalRepository.java
+++
@@ -1,9 +0,0 @@
-package com.KNUFinal.KNUFinal.repository;
-
-import com.KNUFinal.KNUFinal.model.Reservation;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FinalRepository extends JpaRepository<Reservation, Long> {
-}
--- finalproject-main/finalproject-main/backend/src/main/java/com/KNUFinal/KNUFinal/KnuService.java
+++
@@ -1,9 +0,0 @@
-package com.KNUFinal.KNUFinal;
-
-import java.util.List;
-
-public interface KnuService {
-    List<UserDTO> list();
-    public UserDTO findById(long id);
-
-}
